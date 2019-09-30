7c57e20276b8de65cecbec02cc8be658f507d6fd
Khaled_guesmi <khaled.guesmi@esprit.tn>
2019-09-30 16:34:34 +0200

    add scripts and assets
    I added some gun assets
    added a mouvementEnemy Script
    I also added 2 Health scripts a playerHealth + enemyHealth
    deleted an old directory containing an empty scene

---
+++ Assets/AngeliDellaMorte_Assets/Code/Scripts/EnemyHealth.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ADM
+{
+    class EnemyHealth : MonoBehaviour
+    {
+        private float currentHealth = 100;
+        public float CurrentHealth { get; private set; }
+    }
+}
---
+++ Assets/AngeliDellaMorte_Assets/Code/Scripts/PlayerHealth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ADM
+{
+    class PlayerHealth : MonoBehaviour
+    {
+        private float currentHealth = 100;
+        public float CurrentHealth { get; private set; }
+
+    }
+}
